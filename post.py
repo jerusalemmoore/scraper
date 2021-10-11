c63e270d5f9976efa139f6602dea744b0122ad5f
jerusalemmoore <jerusalemmoore@gmail.com>
2021-10-10 22:13:12 -0700

    scraper now parses all car sales pages

---
+++ post.py
@@ -0,0 +1,6 @@
+class Post:
+    def __init__(self,description, price, location, url):
+        self.description = description
+        self.price = price
+        self.location = location
+        self.url = url
